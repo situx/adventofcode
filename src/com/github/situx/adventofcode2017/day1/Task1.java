f5718fba0802bca05989c19a4522fa4ca496e9d3
Timo <timo.homburg@hs-mainz.de>
2017-12-01 10:45:52 +0100

    Day1.1
    Solved Day1 Task 2

--- src/com/github/situx/adventofcode2017/day1/Task1.java
+++ src/com/github/situx/adventofcode2017/day1/Task1.java
@@ -12,26 +12,24 @@ import java.nio.file.Paths;
 public class Task1 {
 	
 	/**
-	 * Scans a String for reoccuring characters and if two characters match up adds the Integer value of the first one to a sum.
+	 * Scans a String for character matches using a predefined offset.
 	 * @param numString the String to examine
 	 * @return the sum
 	 */
-	public Integer scanString(String numString){
+	public Integer scanString(String numString,Integer offset){
 		Character curchar,nextchar;
 		Integer sum=0;
-		for(int i=0;i<numString.length()-1;i++){
+		for(int i=0;i<numString.length();i++){
 			curchar=numString.charAt(i);
-			nextchar=numString.charAt(i+1);
+			nextchar=numString.charAt((i+offset)%numString.length());
 			if(curchar==nextchar){
 				System.out.println("Match "+curchar+" - "+nextchar+" CurrentSum: "+sum);
 				sum+=Integer.valueOf(curchar.toString());
 			}
 		}
-		if(numString.charAt(0)==numString.charAt(numString.length()-1)){
-			sum+=Integer.valueOf(numString.charAt(0)+"");
-		}
 		return sum;
 	}
+	
 	
 	/**
 	 * Main method of this program.
@@ -41,7 +39,8 @@ public class Task1 {
 	public static void main(String[] args) throws IOException{
 		Task1 task1=new Task1();
 		String numString=new String(Files.readAllBytes(Paths.get("task1.txt")));
-		System.out.println("Result: "+task1.scanString(numString));
+		System.out.println("Result: "+task1.scanString(numString,1));
+		System.out.println("Result: "+task1.scanString(numString,(numString.length()/2)%numString.length()));
 	}
 	
 }
